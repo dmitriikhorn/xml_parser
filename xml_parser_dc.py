7cb3a7f919eaf09612871a48f142c71e5905e871
dmitriikhorn <dima.khorn@gmail.com>
2023-08-06 15:41:11 +0200

    Add post init checkup for dataclasses

--- xml_parser_dc.py
+++ xml_parser_dc.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
-from typing import Any
+from typing import Any, Optional
+import pydantic
 from pydantic import BaseModel
+from xml_parser_exceptions import IncorrectXmlParserApiRequest
 
 
 @dataclass
@@ -12,21 +14,32 @@ class FilterElement:
     2. filter_path: None (applied to the current root)
     /profile[re:match(text(), ".*")]
     """
-    regexp: str = '.*'
-    filter_path: str = None
-    indexed_query: str = ""
-    unindexed_path: str = ""
+    filter_path: str | None = None
+    regexp: str | None = None
+    indexed_query: str | None = None
+    unindexed_path: str | None = None
+    is_a_path: bool = True
+
+    def __post_init__(self) -> None:
+        if not self.filter_path:
+            self.filter_path = "text()"
+            self.is_a_path = False
+        if not self.regexp:
+            self.regexp = ".*"
 
 
 @dataclass
 class PathElement:
-    name: str = ''
-    full_name: str = ''
-    sibling_id: int = None
+    name: str | None = None
+    sibling_id: int | None = None
     filters: list[FilterElement] = field(default_factory=list)
-    indexed_path: str = ''
+    indexed_path: str | None = None
+
+    def __post_init__(self) -> None:
+        if not self.name:
+            raise IncorrectXmlParserApiRequest(f"Name of the element in the XPATH is not provided")
 
 
 class ResultItem(BaseModel):
-    path_attribute: str = ''
+    path_attribute: str = ""
     value: Any = None
