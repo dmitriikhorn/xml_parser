ea6e40aae705f19d1a295aa4c1dacfa381059ce1
dmitriikhorn <dima.khorn@gmail.com>
2023-08-14 18:12:05 +0200

    Substitute dataclasses with pydantic BaseModel

--- xml_parser_dc.py
+++ xml_parser_dc.py
@@ -1,45 +1,54 @@
 from dataclasses import dataclass, field
 from typing import Any, Optional
 import pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel,root_validator, validator
 from xml_parser_exceptions import IncorrectXmlParserApiRequest
 
 
-@dataclass
-class FilterElement:
+class FilterElement(BaseModel):
     """
     For example:
     1. filter_path: user-profile-parent/user-profile-child
-    /profile[re:match(user-profile-parent/user-profile-child, ".*")]
-    2. filter_path: None (applied to the current root)
-    /profile[re:match(text(), ".*")]
+    /profile[re:match(user-profile-parent/user-profile-child, "regex")]
+    2. filter_path: (applied to the current root) /profile[re:match(text(), ".*")]
     """
-    filter_path: str | None = None
-    regexp: str | None = None
-    indexed_query: str | None = None
-    unindexed_path: str | None = None
+    filter_path: str
+    regexp: str
     is_a_path: bool = True
+    indexed_query: str | None
+    unindexed_path: str | None
 
-    def __post_init__(self) -> None:
-        if not self.filter_path:
-            self.filter_path = "text()"
+    def __init__(self, **values: dict[str]):
+        super().__init__(**values)
+        if not values.get("filter_path"):
             self.is_a_path = False
-        if not self.regexp:
-            self.regexp = ".*"
+
+    @validator("regexp")
+    @classmethod
+    def validate_regexp(cls, value: str) -> str:
+        return value or ".*"
+
+    @validator("filter_path")
+    @classmethod
+    def validate_filter_path(cls, value: str) -> str:
+        return value or "text()"
 
 
-@dataclass
-class PathElement:
-    name: str | None = None
-    sibling_id: int | None = None
-    filters: list[FilterElement] = field(default_factory=list)
-    indexed_path: str | None = None
+class PathElement(BaseModel):
+    name: str
+    sibling_id: int | None
+    filters: list[FilterElement] = list()
+    indexed_path: str | None
 
-    def __post_init__(self) -> None:
-        if not self.name:
+    @validator("name")
+    @classmethod
+    def validate_name(cls, value: str | None) -> str:
+        if not value:
             raise IncorrectXmlParserApiRequest(f"Name of the element in the XPATH is not provided")
+        return value
 
 
 class ResultItem(BaseModel):
     path_attribute: str = ""
     value: Any = None
+
